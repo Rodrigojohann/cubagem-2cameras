a9f3845df73ad9470435c30a7a87111a442ed177
rodrigo <rodrigo@debian.debian>
2021-12-29 09:10:00 -0300

    test

--- src/sensor.cpp
+++ src/sensor.cpp
@@ -79,7 +79,9 @@ PointCloudT::Ptr Sensor::TwoCamStream(char* ipAddress1, char* ipAddress2, unsign
     PointCloudT::Ptr                  cloud_transformed (new PointCloudT);
     PointCloudT::Ptr                  cloud_aligned (new PointCloudT);
     PointCloudT::Ptr                  output_cloud (new PointCloudT);
-    pcl::PassThrough<pcl::PointXYZ>                passz;
+    PointCloudT::Ptr                  cloud_transformed1 (new PointCloudT);
+    PointCloudT::Ptr                  cloud_transformed2 (new PointCloudT);
+    pcl::PassThrough<pcl::PointXYZ>   passz;
 ////
     cloud_raw1.reset(new pcl::PointCloud<pcl::PointXYZ>);
     cloud_raw2.reset(new pcl::PointCloud<pcl::PointXYZ>);
@@ -104,16 +106,40 @@ cout << "\n\nCloud 1 filtered: " << cloud_raw1->points.size();
 cout << "\nCloud 2 filtered: " << cloud_raw2->points.size();
 
 
+float theta = M_PI;
+Eigen::Affine3f transform_2 = Eigen::Affine3f::Identity();
+transform_2.rotate (Eigen::AngleAxisf (theta, Eigen::Vector3f::UnitZ()));
+
+std::cout << transform_2.matrix() << std::endl;
+
+pcl::transformPointCloud (*cloud_raw1, *cloud_transformed, transform_2);
+
+
+pcl::MomentOfInertiaEstimation <pcl::PointXYZ> feature_extractor;
 
 
 
-//float theta = M_PI;
-//Eigen::Affine3f transform_2 = Eigen::Affine3f::Identity();
-//transform_2.rotate (Eigen::AngleAxisf (theta, Eigen::Vector3f::UnitZ()));
+pcl::PointXYZ min_point_OBB1;
+pcl::PointXYZ max_point_OBB1;
+pcl::PointXYZ position_OBB1;
+Eigen::Matrix3f rotational_matrix_OBB1;
 
-//std::cout << transform_2.matrix() << std::endl;
+feature_extractor.setInputCloud (cloud_transformed);
+feature_extractor.compute ();
+feature_extractor.getOBB (min_point_OBB1, max_point_OBB1, position_OBB1, rotational_matrix_OBB1);
 
-//pcl::transformPointCloud (*cloud_raw1, *cloud_transformed, transform_2);
+pcl::PointXYZ min_point_OBB2;
+pcl::PointXYZ max_point_OBB2;
+pcl::PointXYZ position_OBB2;
+Eigen::Matrix3f rotational_matrix_OBB2;
+
+feature_extractor.setInputCloud (cloud_raw2);
+feature_extractor.compute ();
+feature_extractor.getOBB (min_point_OBB2, max_point_OBB2, position_OBB2, rotational_matrix_OBB2);
+
+pcl::transformPointCloud (*cloud_transformed, *cloud_transformed1, rotational_matrix_OBB1.inverse());
+pcl::transformPointCloud (*cloud_raw2, *cloud_transformed2, rotational_matrix_OBB2.inverse());
+
 
 
 //pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
