e020c53eaa0ce5e76852f00d2eb4d9322fc76d95
Rodrigo Johann <Rodrigojohann@github.com>
2022-01-14 18:29:19 +0000

    new parameters

--- src/config.h
+++ src/config.h
@@ -1,8 +1,8 @@
-#define  CAMHEIGHT 3.67
-#define  X_MIN 0.66
-#define  X_MAX 0.62
-#define  Y_MIN 0.5
-#define  Y_MAX 0.8
+#define  CAMHEIGHT 3.5
+#define  X_MIN 1.0
+#define  X_MAX 1.5
+#define  Y_MIN 0.3
+#define  Y_MAX 2.3
 #define IP1 (char*)"192.168.130.191"
 #define IP2 (char*)"192.168.130.195"
 #define PORT 2114
